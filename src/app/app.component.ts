c9366d7fd44baaac26da6d5b5192f9320ecef075
Harshal <harshalghate3131@gmail.com>
2021-10-14 16:25:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Bulb';
+  public OnImage="";
+  public OffImage="";
+  public Result="";
+  BulbOn()
+  {
+    this.Result=this.OnImage="assets/Images/On.png";
+  }
+  BulbOff()
+  {
+    this.Result=this.OffImage="assets/Images/Off.png";
+  }
+
+  ngOnInit()
+  {
+    this.Result=this.OffImage="assets/Images/Off.png";
+  }
+
 }
